e950e85d8d16942e6ea6afc337e1a29b3a3fabd0
Yihang Lin <linyih@sheridancollege.ca>
2023-11-20 15:12:08 -0500

    Fixed Boolean Array logic, toggle working

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -11,7 +11,7 @@ export class ContentComponent {
   cars: Car[] = MYCARS;
   showCars: boolean[] = [];
   displayCar: Car = new Car();
-  i!: number;
+  index!: number;
 
   constructor() {
     // Initialize showCars array with false values
@@ -19,11 +19,19 @@ export class ContentComponent {
   }
 
   getCar(i:number) {
+    if (this.showCars[i] == true){
+      this.showCars[i] = false;
+    }
+    else {
+      this.showCars = this.showCars.fill(false);
     console.log(i, this.cars[i].make);
     this.showCars[i] = !this.showCars[i];
     this.displayCar = this.cars[i];
+    this.index = i;
     console.log('showCars:', this.showCars);
     console.log(this.displayCar.make);
+
+    }
   }
 }
 
